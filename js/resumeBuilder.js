ac63ce961c7097b45d382281b9d7614b59e04645
H-Wardak <6649831+H-Wardak@users.noreply.github.com>
2017-10-03 22:57:43 +0300

    resume builder

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,75 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+*/
+
+var bio = {
+	name : "Haroon Wardak",
+	role : "Security Engineer",
+	contacts : {
+		mobile: "0568877432",
+		email: "myEmail@hotmail.com",
+		github: "H-Wardak",
+		//twitter: string (optional)
+		location: "Riyadh"
+	},
+	welcomeMessage: "Currently, I'm working as pre-sales engineer in Ethergulf enterprise for world class security products such as Symantec Solutions,"+
+	"Proofpoint, Darktrace, LogRhythm, etc.", 
+	skills: ["Java", "Front-end Development", "C#", "Ethical Hacking", "Kali Linux", "Digital Forensics"],
+	biopic: "images\\197x148.gif"
+	//display: function
+};
+
+var education = {
+	schools: [
+	{
+		name: "King Fahd University of Petroleum and Minerals",
+		location: "Dhahran, KSA",
+		degree: "Master",
+		majors: ["Information Assurance and Security"],
+		dates: "2013 - 2017",
+		url: "www.kfupm.edu.sa"
+	}
+	],
+	onlineCourses: [
+	{
+		title: "Front-end Development",
+		school: "Udacity",
+		dates: "Jul 2017 - Oct 2017",
+		url: "www.udacity.com"
+	}
+	],
+	//display: function
+};
+
+var work = {
+	jobs: [
+	{
+           employer: "Ethergulf enterprise", 
+           title: "pre-sales", 
+           location: "Riyadh", 
+           dates: "Feb 2017 - in progress",
+           description: "in charge of maser security products, present it to customer, deploy and support." 
+     },
+     {
+     	employer: "KFUPM", 
+           title: "Research Assistance", 
+           location: "Dhahran", 
+           dates: "Sep 2013 - Feb 2015",
+           description: "Participate in department projects, researchs and teaching labs" 
+     }
+    ],
+      //display: function
+};
+
+var projects = {
+	projects: [{
+		    title: "My portfolio", 
+            dates: "Aug 2017",
+            description: "building a personal portfolio collecting projects I have developed.",
+            images: ["images\\profile-img.png"]
+            },
+            {
+
+            }],
+      //display: function
+};
